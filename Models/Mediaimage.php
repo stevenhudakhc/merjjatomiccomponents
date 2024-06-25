fb2ea566f7db2e9167cf8924d9a398966dc67e0c
stevenhudakhc <80708462+stevenhudakhc@users.noreply.github.com>
2024-06-25 16:14:14 -0400

    Namespace fix

--- Models/Mediaimage.php
+++ Models/Mediaimage.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Stevenhudakhc\Spatietestground\Models;
+namespace Stevenhudakhc\Merjjatomiccomponents\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
