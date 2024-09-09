6554960521a2c6e34be10f2a6fe53fce6933a984
stevenhudakhc <80708462+stevenhudakhc@users.noreply.github.com>
2024-09-09 10:11:44 -0400

    updated integration fixes

--- Models/Atomimage.php
+++ Models/Atomimage.php
@@ -22,17 +22,23 @@ class Atomimage extends Model
         $output_string = '';
         //$row_structure = json_decode($this->structure_json);
 
-        $output_string .= '<img ';
-        $output_string .= " src='".$url_desktop."' ";
-        $output_string .= " class='d-sm-none d-md-block img-fluid' ";
-        $output_string .= '/>';
+        $url_desktop = $this->url_desktop;
+        $url_mobile = $this->url_mobile;
+        if (isset($url_desktop)){
+          $output_string .= '<img ';
+          $output_string .= " src='".$url_desktop."' ";
+          $output_string .= " class='d-sm-none d-md-block img-fluid' ";
+          $output_string .= '/>';
+        }
 
         $output_string .= "\n";
 
-        $output_string .= '<img ';
-        $output_string .= " src='".$url_mobile."' ";
-        $output_string .= " class='d-none d-sm-block d-md-none img-fluid' ";
-        $output_string .= '/>';
+        if (isset($url_mobile)){
+          $output_string .= '<img ';
+          $output_string .= " src='".$url_mobile."' ";
+          $output_string .= " class='d-none d-sm-block d-md-none img-fluid' ";
+          $output_string .= '/>';
+        }
 
         return $output_string;
     }
